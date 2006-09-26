e8b9050ed7c96fe63217edaae2b210f7364accbc
mpictor <mpictor@2359b6c2-501e-0410-8cfe-a57835d51191>
2006-09-26 03:16:18 +0000

    Fix run.sh
    git-svn-id: https://cam-occ.googlecode.com/svn@3 2359b6c2-501e-0410-8cfe-a57835d51191

--- trunk/src/run.sh
+++ trunk/src/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=../lib
+echo If this fails, set CASROOT to the correct location, 
+echo usually /opt/OpenCASCADE6.1/ros
+export LD_LIBRARY_PATH=$CASROOT/lib
 ./cam-occ
 
